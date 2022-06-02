35ae3cb5b6b76807f08e7fe5a7c65c493706fdb8
Do Minh Khai <khaihkd@gmail.com>
2022-06-02 15:37:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ mv gg18_sign_client /usr/local/bin/
 
 echo "setup environments"
 mkdir ${DIR}
-echo '{"parties":"11", "threshold":"7"}' >> ${DIR}/params.json
+echo '{"parties":"5", "threshold":"3"}' >> ${DIR}/params.json
 sudo tee ${DIR}/generateKeyStore.sh <<EOF
 #!/usr/bin/env bash
 cd ${DIR} && gg18_keygen_client $1 ${DIR}/keys.store
@@ -35,26 +35,7 @@ EOF
 
 
 chmod +x ${DIR}/generateKeyStore.sh
-
-echo 'init service'
-sudo tee /etc/systemd/system/dotoracleGenerateKeyStore.service <<EOF
-[Unit]
-Description=DotOracle Generate Key Store
-After=network-online.target
-Requires=network-online.target
-[Service]
-ExecStart=${DIR}/generateKeyStore.sh
-LimitAS=infinity
-LimitRSS=infinity
-LimitCORE=infinity
-LimitNOFILE=6553600
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOF
-
-echo 'start service'
-sudo systemctl enable dotoracleGenerateKeyStore.service && sudo systemctl start dotoracleGenerateKeyStore.service
+bash ${DIR}/generateKeyStore.sh
 
 
 echo "Waiting other validator generate key... Your key will create when all validator run this script. It will store at ${DIR}/keys.store. You need backup this file"
